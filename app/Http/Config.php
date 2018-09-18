87cbb7f4a2d64bf217f5177603d12b5c1b228485
Javzero <Javzero1@gmail.com>
2018-09-18 05:47:05 -0300

    Fix

--- app/Http/Config.php
+++ app/Http/Config.php
@@ -1,5 +1,5 @@
 <?php 
 
-    define('APP_EMAIL_1','dev@vimana.studio');
+    define('APP_EMAIL_1','info@vimana.studio');
     define('APP_EMAIL_2','dev@vimana.studio');
     define('APP_BUSSINESS_NAME', 'Vimana Studio');
