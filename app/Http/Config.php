e3e1522b96135e7fd8452e1ecf9986839e970899
javzero <javzero1@gmail.com>
2019-10-29 19:14:59 -0300

    New portfolio

--- app/Http/Config.php
+++ app/Http/Config.php
@@ -1,5 +1,5 @@
 <?php 
 
-    define('APP_EMAIL_1','info@vimana.studio');
-    define('APP_EMAIL_2','dev@vimana.studio');
+    define('APP_EMAIL_1','info@vimanastudio.com.ar');
+    define('APP_EMAIL_2','info@vimanastudio.com.ar');
     define('APP_BUSSINESS_NAME', 'Vimana Studio');
